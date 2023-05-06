2f59628f1df6e322ed6e967758342ddc6267f546
Rebekah Jeane Hayes <rebekahjhayes@gmail.com>
2023-05-06 10:19:35 -0500

    updated tables

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -1,106 +1,63 @@
-const { KnexTimeoutError } = require('knex');
-const knex = require('../db/connection');
+const knex = require("../db/connection");
 
-function create(newTable) {
-  return knex('tables')
-    .insert({
-      ...newTable,
-    "table_status": newTable.reservation_id ? "occupied" : "free",
+function list(){
+    return knex("tables").select("*")
+    .orderBy("table_name")
+}
+
+function read(table_id){
+    return knex("tables").select("*")
+        .where({table_id: table_id}).first()
+}
+
+function create(table){
+    return knex("tables").insert(table)
+        .returning("*")
+        .then((createdRecords)=> createdRecords[0])
+}
+
+// function to seat a reservaion, updates the reservaion status to seated and adds the reservation id to the table
+function update({ reservation_id, table_id }) {
+    return knex.transaction((trx) => {
+        return knex("reservations")
+            .transacting(trx)
+            .where({ reservation_id })
+            .update({ status: "seated" })
+            .then(() => {
+                return knex("tables")
+                    .transacting(trx)
+                    .where({ table_id })
+                    .update({ reservation_id: reservation_id })
+                    .returning("*")
+            })
+            .then(trx.commit)
+            .catch(trx.rollback);
     })
-    .returning('*')
-    .then((result) => result[0]);
 }
 
-function read(table_id) {
-  return knex("tables as t")
-    .leftJoin("reservations as r", "r.reservation_id", "t.reservation_id")
-    .select(
-      "t.table_id",
-      "t.table_name",
-      "t.capacity",
-      "t.reservation_id",
-      "r.first_name",
-      "r.last_name",
-      "r.mobile_number",
-      "r.reservation_date",
-      "r.reservation_time",
-      "r.people",
-      "r.status",
-      "r.created_at as reservation_created",
-      "r.updated_at as reservation_updated"
-    )
-    .where({ table_id })
-    .then((result) => result[0]);
-}
-
-function readTable(table_id) {
-  return knex('tables as t')
-    .select('*')
-    .where({ table_id })
-    .first();
-}
-
-function readReservation(reservation_id) {
-  return knex('reservations as r')
-    .select('*')
-    .where({ reservation_id })
-    .first();
-}
-
-function readTableByRes(reservation_id) {
-  return knex("tables")
-    .where({ reservation_id })
-    .whereExists(knex.select("*").from("tables").where({ reservation_id }))
-    .then((result) => result[0]);
-}
-
-function list() {
-  return knex('tables')
-    .select('*')
-    .orderBy('table_name');
-}
-
-async function updateSeatRes(reservation_id, table_id) {
-  const trx = await knex.transaction();
-  return trx("tables")
-    .where({ table_id })
-    .update({ 
-      reservation_id: reservation_id,
-      table_status: "occupied",
-    }, "*")
-    .then(() => 
-      trx("reservations")
-      .where({ reservation_id })
-      .update({ status: "seated" })
-    )
-    .then(trx.commit)
-    .catch(trx.rollback);
-}
-
-async function destroyTableRes(table_id, reservation_id) {
-  const trx = await knex.transaction();
-  return trx("tables")
-    .where({ table_id })
-    .update({
-      reservation_id: null,
-      table_status: "free",
-    }, "*")
-    .then(() => 
-      trx("reservations")
-      .where({ reservation_id })
-      .update({ status: "finished" }, "*")
-    )
-    .then(trx.commit)
-    .catch(trx.rollback)
+// function for a done reservation, updates the reservaion status to finishes and delete the reservation id from the table
+function finished({ table_id, reservation_id }) {
+    return knex.transaction((trx) => {
+        return knex("reservations")
+            .transacting(trx)
+            .where({ reservation_id })
+            .update({ status: "finished" })
+            .then(() => {
+                return knex("tables")
+                    .transacting(trx)
+                    .where({ table_id })
+                    .update({ reservation_id: null })
+                    .returning("*")
+            })
+            .then(trx.commit)
+            .catch(trx.rollback);
+    })
 }
 
 module.exports = {
-  create,
-  list,
-  read,
-  readTable,
-  readReservation,
-  readTableByRes,
-  updateSeatRes,
-  destroyTableRes,
-}
+    create,
+    list,
+    read,
+    update,
+    finished,
+};
